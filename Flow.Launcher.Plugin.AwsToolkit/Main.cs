6b49ff26a6c4c1a61046f03d0028164da689fe1c
Michael Timblin <mjtimblin@gmail.com>
2022-05-26 00:50:38 -0500

    Convert Wox plugin to Flow Launcher plugin

---
+++ Flow.Launcher.Plugin.AwsToolkit/Main.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Flow.Launcher.Plugin.SharedCommands;
+
+namespace Flow.Launcher.Plugin.AwsToolkit
+{
+    public class Main: IPlugin
+    {
+        private string _pluginDirectory;
+
+        private List<ConsoleService> _services;
+
+        public List<Result> Query(Query query)
+        {
+            var results = new List<Result>();
+            if (string.IsNullOrEmpty(query.Search)) return results;
+            var keyword = query.Search;
+
+            var searchList = _services.Where(x => x.command.ToLower().Contains(keyword));
+
+            results.AddRange(searchList.Select(s => new Result
+            {
+                Title = s.command,
+                SubTitle = s.description,
+                IcoPath = $"{_pluginDirectory}\\images\\{s.command}.png",
+                Action = c =>
+                {
+                    try
+                    {
+                        s.url.OpenInBrowserWindow();
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        return false;
+                    }
+                },
+                ContextData = s
+            }));
+
+            return results;
+        }
+
+        public void Init(PluginInitContext context)
+        {
+            _pluginDirectory = context.CurrentPluginMetadata.PluginDirectory;
+
+            _services = File.ReadLines($"{_pluginDirectory}\\console-services.jsonl").ToList().ConvertAll(
+                line => JsonSerializer.Deserialize<ConsoleService>(line));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-# Wox.Plugin.AwsToolkit
-A Wox plugin for working with AWS.
+# AWS Toolkit 
 
-Heavily inspired by the Alfred workflow: https://github.com/rkoval/alfred-aws-console-services-workflow
+## Installation
+
+This plugin is intended to be used by [Flow Launcher](https://www.flowlauncher.com/). It will not work with Wox.
+
+To download this plugin, go to the [latest release](https://github.com/mjtimblin/Flow.Launcher.Plugin.AwsToolkit/releases/latest) and extract the zip file to Flow's user data plugin directory.
+
+## Acknowledgments
+
+This is a port of the Wox plugin [Wox.Plugin.AwsToolkit](https://github.com/mikemorain/Wox.Plugin.AwsToolkit) (AWS Toolkit) created by Mike Morain ([@mikemorain](https://github.com/mikemorain)).
+
+The Wox plugin, and therefore this plugin as well, was heavily inspired by the Alfred workflow: [AWS Console Services](https://github.com/rkoval/alfred-aws-console-services-workflow) created by Ryan Koval ([@rkoval](https://github.com/rkoval)).
