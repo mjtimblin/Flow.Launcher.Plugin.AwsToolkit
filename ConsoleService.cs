4d9bb3b566526e3d046b4986dd341dbc8cf1cdde
Michael Timblin <mjtimblin@gmail.com>
2022-05-27 03:04:56 -0500

    Fix compiler warnings by adding comments and changing the case of JSON attributes

--- ConsoleService.cs
+++ ConsoleService.cs
@@ -1,9 +1,23 @@
 ﻿namespace Flow.Launcher.Plugin.AwsToolkit
 {
+    /// <summary>
+    /// A data class for information about an AWS Console service
+    /// </summary>
     public class ConsoleService
     {
-        public string command { get; set; }
-        public string description { get; set; }
-        public string url { get; set; }
+        /// <summary>
+        /// The name of the service (lowercase)
+        /// </summary>
+        public string Command { get; set; }
+
+        /// <summary>
+        /// A short description of the service
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The AWS Console URL for the service
+        /// </summary>
+        public string Url { get; set; }
     }
 }
--- Main.cs
+++ Main.cs
@@ -3,33 +3,41 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
-using Flow.Launcher.Plugin;
 using Flow.Launcher.Plugin.SharedCommands;
 
 namespace Flow.Launcher.Plugin.AwsToolkit
 {
+    /// <summary>
+    /// Main plugin class
+    /// </summary>
     public class Main: IPlugin
     {
         private List<ConsoleService> _services;
 
+        /// <summary>
+        /// Filters AWS Console services by query. Selecting a result opens the AWS Console service URL in a new
+        /// browser tab.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
         public List<Result> Query(Query query)
         {
             var results = new List<Result>();
             if (string.IsNullOrEmpty(query.Search)) return results;
             var keyword = query.Search;
 
-            var searchList = _services.Where(x => x.command.ToLower().Contains(keyword));
+            var searchList = _services.Where(x => x.Command.ToLower().Contains(keyword));
 
             results.AddRange(searchList.Select(s => new Result
             {
-                Title = s.command,
-                SubTitle = s.description,
-                IcoPath = $"Images\\{s.command}.png",
-                Action = c =>
+                Title = s.Command,
+                SubTitle = s.Description,
+                IcoPath = $"Images\\{s.Command}.png",
+                Action = _ =>
                 {
                     try
                     {
-                        s.url.OpenInBrowserTab();
+                        s.Url.OpenInBrowserTab();
                         return true;
                     }
                     catch (Exception)
@@ -42,6 +50,10 @@ namespace Flow.Launcher.Plugin.AwsToolkit
             return results;
         }
 
+        /// <summary>
+        /// Plugin initialization. Reads AWS Console services data from console-service.jsonl 
+        /// </summary>
+        /// <param name="context"></param>
         public void Init(PluginInitContext context)
         {
             _services = File.ReadLines($"{context.CurrentPluginMetadata.PluginDirectory}\\console-services.jsonl").ToList().ConvertAll(
