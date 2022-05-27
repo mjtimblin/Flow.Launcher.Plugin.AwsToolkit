0b8e8ba450d83fec825c7a8bc5f76ff34c7a744d
Michael Timblin <mjtimblin@gmail.com>
2022-05-27 01:45:05 -0500

    Add missing files

---
+++ ConsoleService.cs
@@ -0,0 +1,9 @@
+﻿namespace Flow.Launcher.Plugin.AwsToolkit
+{
+    public class ConsoleService
+    {
+        public string command { get; set; }
+        public string description { get; set; }
+        public string url { get; set; }
+    }
+}
