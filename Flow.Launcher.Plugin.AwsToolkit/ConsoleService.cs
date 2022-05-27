d547b3e3c20b863e42a0f17607e403f15d521b5b
Michael Timblin <mjtimblin@gmail.com>
2022-05-27 01:25:17 -0500

    Remove unnecessary folder by moving the .csproj and other source code to the root of the repo

--- Flow.Launcher.Plugin.AwsToolkit/ConsoleService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Flow.Launcher.Plugin.AwsToolkit
-{
-    public class ConsoleService
-    {
-        public string command { get; set;  }
-        public string description { get; set; }
-        public string url { get; set; }
-    }
-}
